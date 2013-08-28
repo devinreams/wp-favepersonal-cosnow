1543e21541ca6cc9c6178a6ef0b1595f8bde7532
Devin Reams <devin@reams.me>
2013-08-28 17:55:47 -0600

    change the feature posts to an aside for SEO

---
+++ header/featured-posts.php
@@ -0,0 +1,11 @@
+<aside id="masthead">
+	<div class="container clearfix">
+		<div id="featured-posts" class="clearfix cf-clearfix">
+<?php
+
+echo $content;
+
+?>
+		</div>
+	</div><!-- .container -->
+</aside><!-- #masthead -->
