91d8d273e6d03352fe264f92264864618de34ffa
Devin Reams <devin@reams.me>
2017-01-29 07:44:49 -0700

    header with upcoming events

--- header/featured-posts.php
+++ header/featured-posts.php
@@ -1,18 +1,16 @@
 <aside id="masthead">
 	<div class="container clearfix">
 		<div id="featured-posts" class="clearfix cf-clearfix">
-<?php
-
-echo $content;
-wp_reset_query();
-
-?>
+			<?php
+			echo $content;
+			wp_reset_query();
+			?>
 		</div>
 	</div><!-- .container -->
     <div id="header-ads" style="margin-top:30px;">
             <center>
-              <span id="header-ad-1"><script type="text/javascript" src="//www.avantlink.com/link.php?ml=423513&amp;p=105541&amp;pw=125397&amp;open=_blank"></script></span>
-              <span id="header-ad-2"><script type="text/javascript" src="//www.avantlink.com/link.php?ml=29213&amp;p=105541&amp;pw=125397&amp;open=_blank"></script></span>
+              <span id="header-ad-1" onclick="_gs('event', 'Clicked Header Ad 1')"><script type="text/javascript" src="//www.avantlink.com/link.php?ml=423513&amp;p=105541&amp;pw=125397&amp;open=_blank"></script></span>
+              <span id="header-ad-2" onclick="_gs('event', 'Clicked Header Ad 2')"><script type="text/javascript" src="//www.avantlink.com/link.php?ml=29213&amp;p=105541&amp;pw=125397&amp;open=_blank"></script></span>
             </center>
     </div>
 </aside><!-- #masthead -->
--- header/header-default.php
+++ header/header-default.php
@@ -45,7 +45,16 @@ var _prum = [['id', '53e7b91eabe53d972aa4aa51'],
     s.parentNode.insertBefore(p, s);
 })();
 </script>
+	<link rel="mask-icon" href="http://cosnow.com/wp-content/themes/cosnow/safari-pinned-tab.svg" color="#294c6d">
+	<meta name="theme-color" content="#ffffff">
 	<?php wp_head(); ?>
+	<script>
+	  !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(
+	  arguments)};d=s.createElement(q);q=s.getElementsByTagName(q)[0];
+	  d.src='//d1l6p2sc9645hc.cloudfront.net/tracker.js';q.parentNode.
+	  insertBefore(d,q)}(window,document,'script','_gs');
+	  _gs('GSN-300719-N');
+	</script>
 </head>
 <body <?php body_class(); ?>>
 	<header id="header">
@@ -68,6 +77,7 @@ var _prum = [['id', '53e7b91eabe53d972aa4aa51'],
 
 	<section id="content">
 		<div class="container clearfix">
-			<div id="follow-on-apple-news">
-				<a href="https://apple.news/T3CtnoBLBQHKnwLJLlmJehg"><img src="http://cosnow.com/wp-content/themes/cosnow/images/Follow_on_AppleNews.png" title="Follow Colorado Snow on Apple News" style="height: 19px" height="19px"></a>
-			</div>
+			<p style="background:#d4efff;padding:10px;margin-bottom:20px;"><strong><a href="/events/">Upcoming Events:</a></strong>
+				<?php $loop = new WP_Query( array( 'post_type' => 'tribe_events', 'posts_per_page' => 2 ) ); ?>
+				<?php while ( $loop->have_posts() ) : $loop->the_post(); ?><?php if($loop->current_post!=0) echo ", "; ?><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a><?php endwhile; wp_reset_query(); ?>
+			</p>
